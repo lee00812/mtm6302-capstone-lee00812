d272ece57e30e9171f5bd8ee37dfffa828d41268
Gloria Lee <lee00812@algonquinlive.com>
2023-04-06 13:03:29 -0400

    adds display abilities

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
+//display 20 pokemons
 const $users = document.getElementById('list')
-let $url = ''
+let $next = ''
+let $previous = ''
 
 // pokemon API
 async function fetchData(url) {
 
     const response = await fetch(url);
     const json = await response.json();
-    $url = json.next
+    $next = json.next
+    $previous = json.previous
 
     displayPokemons(json.results);
   }
   
   fetchData('https://pokeapi.co/api/v2/pokemon/');
-
   
+//display pokemon's image and name
   function displayPokemons(pokemons) {
   
     const htmlTemplate = [];
     for (const pokemon of pokemons) {
   
-   
       const $img = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + parseUrl(pokemon.url) + '.png'
   
       htmlTemplate.push(
         `
-        <div class="pokecontainer">
+        <div class="pokecontainer" id="pokemon">
         <img src="${$img}"><li>${pokemon.name}</li>
         </div>
         `
@@ -33,7 +35,7 @@ async function fetchData(url) {
   
     $users.innerHTML = htmlTemplate.join('');
   }
-  
+
   // parseURL
   // Will return the pokemon's id from the provided url
   function parseUrl (url) {
@@ -42,14 +44,54 @@ async function fetchData(url) {
 
   //add event listner
   
-  const button = document.getElementById('load-more')
+  const nextButton = document.getElementById('next')
 
 // using an anonymous function
-    button.addEventListener('click', function () {
-        fetchData($url)
-        console.log(`The button was clicked`)
+    nextButton.addEventListener('click', function () {
+        fetchData($next)
+        // console.log(`The button was clicked`)
     })
 
+    const previousButton = document.getElementById('previous')
+
+// using an anonymous function
+    previousButton.addEventListener('click', function () {
+        fetchData($previous)
+        // console.log(`The button was clicked`)
+    })
+
+//display pokemon's abilities
+//adds interactive scripts
+
+const $displayPokemon = document.querySelectorAll('#pokemon')
+const $screen = document.getElementById('display')
+
+$displayPokemon.forEach(function (pokemon) {
+    pokemon.addEventListener('click', function () {
+        $screen.innerHTML = 
+        `
+        <div class="display-image">
+        <a></a>  
+        </div>
+        <div class="display-text">
+            <h3 class="pt-4 pb-2">Name</h3>
+            <p>abilities</p>
+        </div>
+        `
+    })
+})
+
+
+// const $displayPokemon = document.getElementById('pokemon')
+// const $screen = document.getElementById('screen')
+
+// for (let i = 0; i < 20; i++) {
+//     $displayPokemon[i].addEventListener('click', function () {
+//         $screen.innerHTML = `
+//         <p>Testing Testing</p>`
+//     })
+    
+//   }
 
 
   
