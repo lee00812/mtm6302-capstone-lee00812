d9f82a4416eda38cc3f1a5c3b357f23f470d05fd
Gloria Lee <lee00812@algonquinlive.com>
2023-04-14 14:49:27 -0400

    adds event listener to select and unselect pokemon

--- script.js
+++ script.js
@@ -77,12 +77,15 @@ async function fetchAbilityName(id, name) {
   const details = []
 
   details.push(`
+    
+    <div class="display-text">
+      <h3 class="px-2 bg-danger text-light text-uppercase">${name}</h3>
+    </div>
     <div class="display-image">
       <img src="${imgUrl}" alt="Pokemon">
     </div>
     <div class="display-text">
-      <h3 class="pt-4 pb-2">${name}</h3>
-      <h4>Abilities:</h4>
+      <h4 class="abilities">Abilities:</h4>
       <ul>
   `)
 
@@ -108,6 +111,37 @@ document.getElementById('list').addEventListener('click', function (e) {
 
   }
 }, false);
+
+//event listener to the select button
+//add pokemon to the selected list
+
+const selectPokemon = document.getElementById('selectpokemon');
+const selectedPokemon = document.getElementById('selectedpokemon');
+
+selectPokemon.addEventListener('click', function (e) {
+  const selectedName = e.target.dataset.name;
+  const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${e.target.dataset.id}.png`;
+  const imgName = e.target.dataset.name;
+
+  selectedPokemon.innerHTML = `
+    <div class="selectedlist">
+      <img src="${imgUrl}" alt="Pokemon">
+    </div>
+    <div class="display-text">
+      <h3 class="pt-4 pb-2">${imgName}</h3>
+    </div>
+  `;
+});
+
+
+//release pokemon
+// const releasePokemon = document.getElementById('release')
+
+// releasePokemon.addEventListener('click', function (e) {
+ 
+// }
+
+// )
 
 
 
